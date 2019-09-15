1a07aab3535014b85cacbbaacb4d1e3ffcfeaa90
unknown <anand.vardhan@SFDC>
2019-09-15 10:48:56 +0530

    This is my second commit

--- Action.c
+++ Action.c
@@ -1,6 +1,7 @@
 Action()
 {
-	
+	//This is my first GIT Interaction with LoadRunner
+	//I will be doing first pull and commit
 	web_custom_request("web_custom_request",
 		"URL=www.example.com",
 		"Method=GET",
